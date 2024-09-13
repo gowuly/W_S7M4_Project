a2cd0cc58c79b7940ee90c7223922d561d4e0a0a
Wulnan Shadrach <wulnanshadrach268@gmail.com>
2024-09-13 21:52:40 +0100

    functions and component testing with Jest and React Testing Library functional

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import * as yup from 'yup'
 import schemas from '../../shared/schemas'
+import txt from '../i18n/index.json'
 
 /*
   👉 TASK 2
@@ -26,6 +27,7 @@ export default function App({ lang = 'en' }) {
   const [success, setSuccess] = useState()
   const [failure, setFailure] = useState()
   const [submitAllowed, setSubmitAllowed] = useState(false)
+  const texts = txt[language]
 
   useEffect(() => {
     schemas.userSchema.isValid(values).then(setSubmitAllowed)
@@ -64,7 +66,7 @@ export default function App({ lang = 'en' }) {
   return (
     <div>
       <h2>
-        Create an Account
+        {texts.TEXT_HEADING_CREATE_ACCOUNT}
         <span onClick={() => setLanguage(language === 'en' ? 'esp' : 'en')}>
           {language === 'en' ? ' 🇺🇸' : ' 🇪🇸'}
         </span>
@@ -74,21 +76,21 @@ export default function App({ lang = 'en' }) {
         {failure && <h4 className="error">{failure}</h4>}
 
         <div className="inputGroup">
-          <label htmlFor="username">Username:</label>
+          <label htmlFor="username">{texts.LABEL_USERNAME}</label>
           <input id="username" name="username" onChange={onChange} value={values.username} type="text" placeholder="Type username" />
           {errors.username && <div className="validation">{errors.username}</div>}
         </div>
 
         <div className="inputGroup">
           <fieldset>
-            <legend>Favorite language:</legend>
+            <legend>{texts.TEXT_FAV_LANG}</legend>
             <label>
               <input onChange={onChange} type="radio" name="favLanguage" value="javascript" checked={values.favLanguage == 'javascript'} />
-              JavaScript
+              {texts.TEXT_FAV_LANG_JS}
             </label>
             <label>
               <input onChange={onChange} type="radio" name="favLanguage" value="rust" checked={values.favLanguage == 'rust'} />
-              Rust
+              {texts.TEXT_FAV_LANG_RUST}
             </label>
           </fieldset>
           {errors.favLanguage && <div className="validation">{errors.favLanguage}</div>}
--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App'
+import txt from '../i18n/index.json'
 
 describe('Module 4 Project Tests', () => {
   describe('English Language', () => {
@@ -10,12 +11,47 @@ describe('Module 4 Project Tests', () => {
 
       One test is done for you as an example.
     */
-    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
+
+      let texts = getEntriesByKeyPrefix(txt.en, 'TEXT')
+      for(let [key, val] of texts){
+        test(`${key} is vesible`, 9)
+      }
+   // test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
+     // render(<App lang="en" />)
+      //expect(screen.getByText(txt.en.TEXT_HEADING_CREATE_ACCOUNT)).toBeVisible()
+    //})
+
+    //LABEL_USERNAME
+    test(`LABEL_USERNAME is visible`, () => {
       render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
+      expect(screen.getByLabelText(txt.en.LABEL_USERNAME)).toBeVisible()
     })
+
+    //PLACEHOLDER_USERNAME
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByPlaceholderText(txt.en.PLACEHOLDER_USERNAME)).toBeVisible()
+    })
+
   })
   describe('Spanish Language', () => {
+
+    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(txt.esp.TEXT_HEADING_CREATE_ACCOUNT)).toBeVisible()
+    })
+
+    //LABEL_USERNAME
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByLabelText(txt.esp.LABEL_USERNAME)).toBeVisible()
+    })
+
+    //PLACEHOLDER_USERNAME
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByPlaceholderText(txt.esp.PLACEHOLDER_USERNAME)).toBeVisible()
+    })
     /*
       👉 TASK 3
 
@@ -24,6 +60,31 @@ describe('Module 4 Project Tests', () => {
   })
   describe('getEntriesByKeyPrefix', () => {
     test('can extract the correct data', () => {
+
+     
+
+      const obj ={
+      abc_1: "data_abc_1",
+      abc_2: "data_abc_2",
+      xyz_1: "data_xyz_1",
+      abc_3: "data_abc_3", 
+      }
+
+      const expected = [
+      ["abc_1", "data_abc_1"],
+      ["abc_2", "data_abc_2"],
+      ["abc_3", "data_abc_3"],
+      ]
+
+      const expected2 = [
+        ["xyz_1", "data_xyz_1"],
+      ]
+  
+
+      expect(getEntriesByKeyPrefix(obj, "abc")).toEqual(expected)
+      expect(getEntriesByKeyPrefix(obj, "xyz")).toEqual(expected2)
+      expect(getEntriesByKeyPrefix(obj, "foo")).toEqual([])
+      
     /*
       👉 TASK 4 part 2
 
@@ -39,6 +100,8 @@ describe('Module 4 Project Tests', () => {
   })
 })
 function getEntriesByKeyPrefix(obj, keyPrefix) {
+
+  return Object.entries(obj).filter(([key]) => key.split('_')[0]===keyPrefix)
   /*
     👉 TASK 4 part 1
 
